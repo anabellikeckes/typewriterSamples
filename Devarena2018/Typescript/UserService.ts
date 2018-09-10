c50d3fcf686e050dc656c7f829b40c0bb0f489c5
KeckeAna <akeckes@ekobit.hr>
2018-09-10 18:53:15 +0200

    correction

--- Devarena2018/Typescript/UserService.ts
+++ Devarena2018/Typescript/UserService.ts
@@ -6,10 +6,10 @@
 
 
 export class UserService {
-       constructor(private $http: ng.IHttpService) { }
+      // constructor(private $http: ng.IHttpService) { }
 
        public getAll = () => {
-           return this.$http.get<UserModel>(`$Route`, {  });
+         //  return this.$http.get<UserModel>(`$Route`, {  });
       }
 }
     
